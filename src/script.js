33025f4e1cc4f61de226de7bf6af87b251e0a131
DanniFront <mure.daniela@gmail.com>
2024-12-18 16:28:23 +0100

    Added basic form handling and added typewriter

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  let recipeElement = document.querySelector("#generated-recipe");
+
+  new Typewriter("#generated-recipe", {
+    strings: "Swedish meatballs",
+    autoStart: true,
+    delay: 40,
+    cursor: null,
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
