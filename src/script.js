34b64874b471f1180eadd8abd2bf0095faaeca0b
DanniFront <mure.daniela@gmail.com>
2024-12-19 12:20:43 +0100

    Improved the UX

--- src/script.js
+++ src/script.js
@@ -16,10 +16,14 @@ function generateRecipe(event) {
   let apiKey = "748d5734be19te1af0e3a12aa9abaofd";
   let prompt = `Generate a simple and short swedish recipe about ${instructionsInput.value}`;
   let context =
-    "You are a professional chef and know many simple and short swedish recipes. Please answer in pure html, but don´t write the letters `html` and with seperate lines. You may ONLY write ingredients and not instructions.";
+    "You are a professional chef and know many simple and short swedish recipes. You may ONLY write ingredients and not instructions. Please answer in pure html, but don´t write ```html``` and with seperate lines.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating");
+
+  let recipeElement = document.querySelector("#generated-recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="blink">Searching for a swedish ${instructionsInput.value} recipe for you 😋</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
